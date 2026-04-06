904288c9b81c22baa2c29d1068fbdedb8c1d60fa
Shrish Wadgaonkar <shriwadg@gmail.com>
2026-04-06 11:54:03 +0530

    Updated to exclusive database

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const mongoURI =
-      process.env.MONGO_URI || "mongodb://127.0.0.1:27017/setu-app";
+      process.env.MONGO_URI;
 
     const conn = await mongoose.connect(mongoURI);
 
